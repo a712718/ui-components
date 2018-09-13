f2309c7a6514b3268bb9c5398c08a3ea79e27843
mahong <mahong@supcon.com>
2018-09-13 19:49:25 +0800

    feat: 打包可以使用

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -62,7 +62,7 @@ module.exports = {
   devtool: shouldUseSourceMap ? 'source-map' : false,
   // In production, we only want to load the polyfills and the app code.
   entry: {
-    index: [paths.appProdIndexJs],
+    index: [require.resolve('./polyfills'), paths.appProdIndexJs],
   },
   externals: {
     react: {
@@ -87,10 +87,10 @@ module.exports = {
     libraryTarget: 'umd',
     library: 'UIComponents',
     // Point sourcemap entries to original disk location (format as URL on Windows)
-    // devtoolModuleFilenameTemplate: info =>
-    //   path
-    //     .relative(paths.appSrc, info.absoluteResourcePath)
-    //     .replace(/\\/g, '/'),
+    devtoolModuleFilenameTemplate: info =>
+      path
+        .relative(paths.appSrc, info.absoluteResourcePath)
+        .replace(/\\/g, '/'),
   },
   resolve: {
     // This allows you to set a fallback for where Webpack should look for modules.
@@ -335,81 +335,81 @@ module.exports = {
     // Otherwise React will be compiled in the very slow development mode.
     new webpack.DefinePlugin(env.stringified),
     // Minify the code.
-    // new UglifyJsPlugin({
-    //   uglifyOptions: {
-    //     parse: {
-    //       // we want uglify-js to parse ecma 8 code. However we want it to output
-    //       // ecma 5 compliant code, to avoid issues with older browsers, this is
-    //       // whey we put `ecma: 5` to the compress and output section
-    //       // https://github.com/facebook/create-react-app/pull/4234
-    //       ecma: 8,
-    //     },
-    //     compress: {
-    //       ecma: 5,
-    //       warnings: false,
-    //       // Disabled because of an issue with Uglify breaking seemingly valid code:
-    //       // https://github.com/facebook/create-react-app/issues/2376
-    //       // Pending further investigation:
-    //       // https://github.com/mishoo/UglifyJS2/issues/2011
-    //       comparisons: false,
-    //     },
-    //     mangle: {
-    //       safari10: true,
-    //     },
-    //     output: {
-    //       ecma: 5,
-    //       comments: false,
-    //       // Turned on because emoji and regex is not minified properly using default
-    //       // https://github.com/facebook/create-react-app/issues/2488
-    //       ascii_only: true,
-    //     },
-    //   },
-    //   // Use multi-process parallel running to improve the build speed
-    //   // Default number of concurrent runs: os.cpus().length - 1
-    //   parallel: true,
-    //   // Enable file caching
-    //   cache: true,
-    //   sourceMap: shouldUseSourceMap,
-    // }), // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        parse: {
+          // we want uglify-js to parse ecma 8 code. However we want it to output
+          // ecma 5 compliant code, to avoid issues with older browsers, this is
+          // whey we put `ecma: 5` to the compress and output section
+          // https://github.com/facebook/create-react-app/pull/4234
+          ecma: 8,
+        },
+        compress: {
+          ecma: 5,
+          warnings: false,
+          // Disabled because of an issue with Uglify breaking seemingly valid code:
+          // https://github.com/facebook/create-react-app/issues/2376
+          // Pending further investigation:
+          // https://github.com/mishoo/UglifyJS2/issues/2011
+          comparisons: false,
+        },
+        mangle: {
+          safari10: true,
+        },
+        output: {
+          ecma: 5,
+          comments: false,
+          // Turned on because emoji and regex is not minified properly using default
+          // https://github.com/facebook/create-react-app/issues/2488
+          ascii_only: true,
+        },
+      },
+      // Use multi-process parallel running to improve the build speed
+      // Default number of concurrent runs: os.cpus().length - 1
+      parallel: true,
+      // Enable file caching
+      cache: true,
+      sourceMap: shouldUseSourceMap,
+    }), // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
     new ExtractTextPlugin({
       filename: cssFilename,
     }),
     // Generate a manifest file which contains a mapping of all asset filenames
     // to their corresponding output file so that tools can pick it up without
     // having to parse `index.html`.
-    // new ManifestPlugin({
-    //   fileName: 'asset-manifest.json',
-    // }),
+    new ManifestPlugin({
+      fileName: 'asset-manifest.json',
+    }),
     // Generate a service worker script that will precache, and keep up to date,
     // the HTML & assets that are part of the Webpack build.
-    // new SWPrecacheWebpackPlugin({
-    //   // By default, a cache-busting query parameter is appended to requests
-    //   // used to populate the caches, to ensure the responses are fresh.
-    //   // If a URL is already hashed by Webpack, then there is no concern
-    //   // about it being stale, and the cache-busting can be skipped.
-    //   dontCacheBustUrlsMatching: /\.\w{8}\./,
-    //   filename: 'service-worker.js',
-    //   logger(message) {
-    //     if (message.indexOf('Total precache size is') === 0) {
-    //       // This message occurs for every build and is a bit too noisy.
-    //       return;
-    //     }
-    //     if (message.indexOf('Skipping static resource') === 0) {
-    //       // This message obscures real errors so we ignore it.
-    //       // https://github.com/facebookincubator/create-react-app/issues/2612
-    //       return;
-    //     }
-    //     console.log(message);
-    //   },
-    //   minify: true,
-    //   // For unknown URLs, fallback to the index page
-    //   navigateFallback: publicUrl + '/index.html',
-    //   // Ignores URLs starting from /__ (useful for Firebase):
-    //   // https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
-    //   navigateFallbackWhitelist: [/^(?!\/__).*/],
-    //   // Don't precache sourcemaps (they're large) and build asset manifest:
-    //   staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
-    // }),
+    new SWPrecacheWebpackPlugin({
+      // By default, a cache-busting query parameter is appended to requests
+      // used to populate the caches, to ensure the responses are fresh.
+      // If a URL is already hashed by Webpack, then there is no concern
+      // about it being stale, and the cache-busting can be skipped.
+      dontCacheBustUrlsMatching: /\.\w{8}\./,
+      filename: 'service-worker.js',
+      logger(message) {
+        if (message.indexOf('Total precache size is') === 0) {
+          // This message occurs for every build and is a bit too noisy.
+          return;
+        }
+        if (message.indexOf('Skipping static resource') === 0) {
+          // This message obscures real errors so we ignore it.
+          // https://github.com/facebookincubator/create-react-app/issues/2612
+          return;
+        }
+        console.log(message);
+      },
+      minify: true,
+      // For unknown URLs, fallback to the index page
+      navigateFallback: publicUrl + '/index.html',
+      // Ignores URLs starting from /__ (useful for Firebase):
+      // https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
+      navigateFallbackWhitelist: [/^(?!\/__).*/],
+      // Don't precache sourcemaps (they're large) and build asset manifest:
+      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
+    }),
     // Moment.js is an extremely popular library that bundles large locale files
     // by default due to how Webpack interprets its code. This is a practical
     // solution that requires the user to opt into importing specific locales.
